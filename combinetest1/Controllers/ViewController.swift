97334e4f097dd8d9e7662c6f53180ab025e310d3
Thisura Dodangoda <thisura1998@gmail.com>
2026-01-06 06:41:48 +0530

    Basic layout

--- combinetest1/Controllers/ViewController.swift
+++ combinetest1/Controllers/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var testLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        testLabel.text = "Hello, World!"
     }
-
 
 }
 
